12f4f8f47f18a4016498db4e492e3085f6592b4a
devneto <miguel.neto320@gmail.com>
2023-04-01 16:12:39 -0300

    refactor: extract methods

--- src/2/calculateRide.ts
+++ src/2/calculateRide.ts
@@ -1,18 +1,27 @@
 // @ts-nocheck
 // calculate ride
+
+const isOvernight = (date) => {
+    return date.getHours() >= 22 || date.getHours() <= 6;
+}
+
+const isSunday = (date) => {
+    return date.getDay() === 0;
+}
+
 export function calc(segments) {
     let fare = 0;
     for (const segment of segments) {
         if (segment.distance != null && segment.distance != undefined && typeof segment.distance === "number" && segment.distance > 0) {
             if (segment.date != null && segment.date != undefined && segment.date instanceof Date && segment.date.toString() !== "Invalid Date") {
-                if (segment.date.getHours() >= 22 || segment.date.getHours() <= 6) {
-                    if (segment.date.getDay() !== 0) {
+                if (isOvernight(segment.date)) {
+                    if (!isSunday(segment.date)) {
                         fare += segment.distance * 3.90;
                     } else {
                         fare += segment.distance * 5;
                     }
                 } else {
-                    if (segment.date.getDay() === 0) {
+                    if (isSunday(segment.date)) {
                         fare += segment.distance * 2.9;
                     } else {
                         fare += segment.distance * 2.10;
