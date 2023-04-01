099833cb918a59e9811ae07f372bd19e888492e2
devneto <miguel.neto320@gmail.com>
2023-04-01 16:04:55 -0300

    refactor: naming code smell

---
+++ src/2/calculateRide.ts
@@ -0,0 +1,48 @@
+// @ts-nocheck
+// calculate ride
+export function calc(segments) {
+    let fare = 0;
+    for (const segment of segments) {
+        if (segment.distance != null && segment.distance != undefined && typeof segment.distance === "number" && segment.distance > 0) {
+            if (segment.date != null && segment.date != undefined && segment.date instanceof Date && segment.date.toString() !== "Invalid Date") {
+
+                // overnight
+
+                if (segment.date.getHours() >= 22 || segment.date.getHours() <= 6) {
+
+                    // not sunday
+                    if (segment.date.getDay() !== 0) {
+
+                        fare += segment.distance * 3.90;
+                        // sunday
+                    } else {
+                        fare += segment.distance * 5;
+
+                    }
+                } else {
+                    // sunday
+                    if (segment.date.getDay() === 0) {
+
+                        fare += segment.distance * 2.9;
+
+                    } else {
+                        fare += segment.distance * 2.10;
+                    }
+                }
+            } else {
+                // console.log(d);
+                return -2;
+            }
+        } else {
+            // console.log(distance);
+
+            return -1;
+        }
+
+    }
+    if (fare < 10) {
+        return 10;
+    } else {
+        return fare;
+    }
+}
--- test/2/calculateRide.test.ts
+++ test/2/calculateRide.test.ts
@@ -1,36 +1,36 @@
-import { calc } from "../src/1/main"
+import { calc } from "../../src/2/calculateRide"
 
 test("Should calculate a run at normal time", () => {
-    const fare = calc([{ dist: 10, ds: new Date("2021-03-10T10:00:00") }])
+    const fare = calc([{ distance: 10, date: new Date("2021-03-10T10:00:00") }])
     expect(fare).toBe(21)
 })
 
 test("Should calculate a run at night time", () => {
-    const fare = calc([{ dist: 10, ds: new Date("2021-03-10T22:00:00") }])
+    const fare = calc([{ distance: 10, date: new Date("2021-03-10T22:00:00") }])
     expect(fare).toBe(39)
 })
 
 test("Should calculate a run on Sunday", () => {
-    const fare = calc([{ dist: 10, ds: new Date("2021-03-07T10:00:00") }])
+    const fare = calc([{ distance: 10, date: new Date("2021-03-07T10:00:00") }])
     expect(fare).toBe(29)
 })
 
 test("Should calculate a run on Sunday night", () => {
-    const fare = calc([{ dist: 10, ds: new Date("2021-03-07T22:00:00") }])
+    const fare = calc([{ distance: 10, date: new Date("2021-03-07T22:00:00") }])
     expect(fare).toBe(50)
 })
 
 test("Should not calculate a race with an invalid distance", () => {
-    const fare = calc([{ dist: -10, ds: new Date("2021-03-07T22:00:00") }])
+    const fare = calc([{ distance: -10, date: new Date("2021-03-07T22:00:00") }])
     expect(fare).toBe(-1)
 })
 
 test("Should not calculate a race with an invalid date", () => {
-    const fare = calc([{ dist: 10, ds: new Date("any") }])
+    const fare = calc([{ distance: 10, date: new Date("any") }])
     expect(fare).toBe(-2)
 })
 
 test("Should calculate a race in normal time with minimum value", () => {
-    const fare = calc([{ dist: 3, ds: new Date("2021-03-10T10:00:00") }])
+    const fare = calc([{ distance: 3, date: new Date("2021-03-10T10:00:00") }])
     expect(fare).toBe(10)
 })
