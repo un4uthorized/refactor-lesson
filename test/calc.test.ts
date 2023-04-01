7667ae080b952ac524c29153d230e19b5116a820
devneto <miguel.neto320@gmail.com>
2023-04-01 15:48:53 -0300

    test: should calculate a run at night time

--- test/calc.test.ts
+++ test/calc.test.ts
@@ -1,6 +1,12 @@
 import { calc } from "../src/1/main"
 
 test("Should calculate a run at normal time", () => {
-    const fare = calc([{ dist: 10, ds: new Date("2021-01-01T10:00:00.000Z") }])
+    const fare = calc([{ dist: 10, ds: new Date("2021-03-10T10:00:00") }])
     expect(fare).toBe(21)
 })
+
+test("Should calculate a run at night time", () => {
+    const fare = calc([{ dist: 10, ds: new Date("2021-03-10T22:00:00") }])
+    expect(fare).toBe(39)
+})
+
